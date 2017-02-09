c560b364883b9c3e1395a649e766f072622c0f56
ybzhao <ybzhao@marvell.com>
2017-02-09 20:11:13 +0800

    update case

--- leetcode/easy.c
+++ leetcode/easy.c
@@ -41,3 +41,35 @@ public:
 }
     }
 };
+
+//14. Longest Common Prefix 
+//Write a function to find the longest common prefix string amongst an array of strings. 
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string prefix = "";
+        for(int idx=0; strs.size()>0; prefix+=strs[0][idx], idx++)
+            for(int i=0; i<strs.size(); i++)
+                if(idx >= strs[i].size() ||(i > 0 && strs[i][idx] != strs[i-1][idx]))
+                    return prefix;
+        return prefix;
+    }
+};
+
+//476. Number Complement 
+/*Given a positive integer, output its complement number. The complement strategy is to flip the bits of its binary representation.
+
+Note:
+
+    The given integer is guaranteed to fit within the range of a 32-bit signed integer.
+    You could assume no leading zero bit in the integer’s binary representation.
+*/
+class Solution {
+public:
+    int findComplement(int num) {
+        unsigned mask = ~0;
+        while (num & mask) mask <<= 1;
+        return ~mask & ~num;
+    }
+};
